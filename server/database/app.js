1793ba70bab8e4c34ca67de06923a9ea0962561b
neethuk88 <neetuk88@gmail.com>
2025-03-24 07:23:07 -0400

    made module3 endpoint URLs changes

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,40 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find(); 
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+    try {
+        const state = req.params.state;
+        const documents = await Dealerships.find({ state: state });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+    try {
+        const id = req.params.id;
+        const document = await Dealerships.find({ id: id} )        
+        if (document) {
+            res.json(document);
+        } else {
+            res.status(404).send('Dealer not found');
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 //Express route to insert review
